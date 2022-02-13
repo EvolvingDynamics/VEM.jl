4f13e1f69bb1d6d4e2fb74c411e40ace7635a7b1
Documenter.jl <documenter@juliadocs.github.io>
2022-02-13 19:55:01 +0000

    build based on 2d37205

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VEM","category":"page"},{"location":"#VEM","page":"Home","title":"VEM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Vortex Element Method library for computational fluid dynamics based on the evolution of vorticity","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VEM]","category":"page"}]
+}
