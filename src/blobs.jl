845dc812db2d5d581108f10af59f37f3fe8d5a46
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2022-07-24 02:07:30 +0200

    Add documentation

--- src/blobs.jl
+++ src/blobs.jl
@@ -1,3 +1,40 @@
+"""
+    Blobs{Dimension, ScalarType<:AbstractFloat, CirculationType<:Union{ScalarType, SVector{Dimension, ScalarType}}}
+
+Construct a distribution of 2D or 3D blobs.
+
+The state of the distribution is completely specified by three properties:
+
+ - The circulation carried by the blobs: `circulation`
+ - The core radius of the blobs: `radius`
+ - The coordinates of the blobs: `source`
+
+ Note that circulation is a scalar in 2D and a 3-vector in 3D.
+
+ # Example
+
+ Construct a distribution of two 2D blobs:
+
+    using StaticArrays
+    using VEM
+    
+    circulation = [1.23, 4.56]
+    radius = [0.45, 0.67]
+    source = [SVector(0.0, 0.0), SVector(0.1, 0.1)]
+    
+    blobs = Blobs(circulation, radius, source)
+
+ Construct a distribution of 3D blobs:
+
+     using StaticArrays
+     using VEM
+     
+     circulation = [SVector(1.23, 4.56, 7.89), SVector(3.21, 6.54, 9.87)]
+     radius = [0.54, 0.67]
+     source = [SVector(0.0, 0.0, 0.0), SVector(0.1, 0.1, 0.1)]
+    
+     blobs = Blobs(circulation, radius, source)
+"""
 struct Blobs{Dimension, ScalarType<:AbstractFloat, CirculationType<:Union{ScalarType, SVector{Dimension, ScalarType}}}
     circulation::Vector{CirculationType}
     radius::Vector{ScalarType}
@@ -29,9 +66,77 @@ struct Blobs{Dimension, ScalarType<:AbstractFloat, CirculationType<:Union{Scalar
     end
 end
 
+"""
+    circulation_type(blobs::Blobs)
+
+Determine the type of the circulation carried by a blob.
+
+# Example
+
+    julia> circulation_type(blobs::Blobs{2, Float32})
+    Float32
+    
+    julia> circulation_type(blobs::Blobs{3, Float16})
+    SVector{3, Float16}
+"""
 circulation_type(blobs::Blobs) = eltype(blobs.circulation)
+
+"""
+    radius_type(blobs::Blobs)
+
+Determine the type of the radius of a blob.
+
+# Example
+    
+    julia> radius_type(blobs::Blobs{2, Float32})
+    Float32
+    
+    julia> radius_type(blobs::Blobs{3, Float16})
+    Float16
+"""
 radius_type(blobs::Blobs) = eltype(blobs.radius)
+
+"""
+    source_type(blobs::Blobs)
+
+Determine the type of the source of a blob.
+
+# Example
+
+    julia> source_type(blobs::Blobs{2, Float32})
+    SVector{2, Float32}
+    
+    julia> source_type(blobs::Blobs{3, Float16})
+    SVector{3, Float16}
+"""
 source_type(blobs::Blobs) = eltype(blobs.source)
 
+"""
+    dimension_type(blobs::Blobs)
+
+Determine the dimension of a blob.
+
+# Example
+
+    julia> dimension(blobs::Blobs{2})
+    2
+    
+    julia> dimension_type(blobs::Blobs{3})
+    3
+"""
 dimension(blobs::Blobs) = length(source_type(blobs))
+
+"""
+    scalar_type(blobs::Blobs)
+
+Determine the primitive type used for the data contained by the `Blobs` struct.
+
+# Example
+    
+    julia> scalar_type(blobs::Blobs{2, Float32})
+    Float32
+    
+    julia> scalar_type(blobs::Blobs{3, Float16})
+    Float16
+"""
 const scalar_type = radius_type
--- src/compute_field.jl
+++ src/compute_field.jl
@@ -4,14 +4,62 @@ function kernel_output_type(kernel, blobs, targets)
     return similar_type(promote_type(source_type(blobs), eltype(targets)), kernel_output_size(kernel))
 end
 
-function compute_field(kernel, blobs, targets)
+"""
+    compute_field(kernel, blobs, targets)
+
+Compute a field defined by `kernel` induced by a distribution of `blobs` at `targets`.
+
+The induced field is specified by a `kernel` function.
+The `kernel` computes the induction due to a single blob at a single target.
+The dimension of the output must be defined by extending `kernel_output_size` for the type of the `kernel` function.
+It should take `kernel` as input and return a `StaticArrays.Size`.
+Outputs of arbitrary tensor rank are supported in 2D and 3D.
+
+# Example
+
+Compute the velocity due to three 2D blobs at four targets:
+
+    using LinearAlgebra
+    using StaticArrays
+    using VEM
+
+    function velocity(circulation, radius, source, target)
+        r = target - source
+        rSq = dot(r, r)
+
+        f = cross(circulation, r)/(rSq*pi*2)
+        g = 1 - exp(-rSq/(2*radius^2))
+
+        return f*g
+    end
+
+    VEM.kernel_output_size(::typeof(velocity)) = Size(2)
+
+    circulations = [1.23, 4.56, 7.89]
+    radii = [0.45, 0.90, 1.80]
+    sources = [SVector(-3.0, 4.0), SVector(-5.0, 0.0), SVector(1.0, 2.0)] 
+    blobs = Blobs(circulations, radii, sources)
+
+    targets = [SVector(3.0, 5.0), SVector(-3.0, 5.0), SVector(-3.0, -5.0), SVector(3.0, -5.0)]
+
+    velocity_field = compute_field(velocity, blobs, targets)
+"""
+function compute_field(kernel, blobs::Blobs, targets)
     T = kernel_output_type(kernel, blobs, targets)
     field = zeros(T, length(targets))
     _compute_field!(field, kernel, blobs.circulation, blobs.radius, blobs.source, targets)
     return field
 end
 
-function compute_field!(field, kernel, blobs, targets)
+"""
+    compute_field!(field, kernel, blobs, targets)
+
+In-place version of [`compute_field`](@ref).
+
+The induced field is stored in `field`.
+The output type of `kernel` must be compatible with `eltype(field)`.
+"""
+function compute_field!(field, kernel, blobs::Blobs, targets)
     _compute_field!(field, kernel, blobs.circulation, blobs.radius, blobs.source, targets)
 end
 
