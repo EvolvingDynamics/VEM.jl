6d73b2ce5951140a1545e9a659b47b7ad1aecff9
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2022-07-24 02:56:38 +0200

    Fix type promotion

--- src/linalg.jl
+++ src/linalg.jl
@@ -1,3 +1,3 @@
-@inline function LinearAlgebra.cross(scalar::Number, vector::StaticVector{2})
-    @inbounds return typeof(vector)(-scalar*vector[2], scalar*vector[1])
+@inline function LinearAlgebra.cross(scalar::TS, vector::StaticVector{2, TV}) where {TS<:Number, TV<:Number}
+    @inbounds return similar_type(vector, promote_type(TS, TV))(-scalar*vector[2], scalar*vector[1])
 end
