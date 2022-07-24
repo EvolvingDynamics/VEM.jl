1e5b79981a5e0c4e1afc5a833c4f69310326ac76
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2022-07-24 02:56:09 +0200

    Rename testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 using SafeTestsets  
 
 @safetestset "Vortex blobs" begin include("vortex_blobs.jl") end
-@safetestset "N-body summation" begin include("compute_field.jl") end
+@safetestset "Direct summation" begin include("compute_field.jl") end
